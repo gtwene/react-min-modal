64ba1b354a38f3695777f43d4c1202bc6a086628
Kwame <gtwene001@st.ug.edu.gh>
2022-03-01 11:58:48 +0000

    modal_minimizable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import $ from 'jquery'
+import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    $(document).on('click', '.min', function(){
+      $(this).closest('.modal').find('.content').slideUp();
+      $(this).closest('.modal').animate({'left':0,'bottom':0});
+    });
+
+    $(document).on('click', '.max', function(){
+      $(this).closest('.modal').find('.content').slideDown();
+      $(this).closest('.modal').animate({'left':'20px','bottom':'50%'});
+    });
+
+    $(document).on('click', '.close', function(){
+      $(this).closest('.modal').fadeOut();
+    });
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div class="shield">
+        <div class="modal">
+          <div class="header">
+            <span>Modal Window</span>
+            <span class="buttons">
+              <span class="min">_</span>
+              <span class="max">[ ]</span>
+              <span class="close">X</span>
+            </span>
+          </div>
+          <div class="content">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate
+            corrupti eum autem unde similique animi harum tempora, voluptatem
+            ratione blanditiis dolores debitis modi. Reiciendis nihil debitis
+            repellendus error necessitatibus impedit.
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
